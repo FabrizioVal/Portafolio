70a16e0882f6857a211dd0a14a74a3fb5b901543
FabriVal <fabriziogabrielvallone@gmail.com>
2024-03-12 16:29:02 -0300

    primera implementacion de splash screen 12 03 2024

--- Portafolio/src/components/start-portfolio.jsx
+++ Portafolio/src/components/start-portfolio.jsx
@@ -1,19 +1,36 @@
-// StartPortfolio.jsx
 import React from 'react';
+import { motion } from 'framer-motion';
+import cat from '../assets/cat.gif';
 
 const StartPortfolio = () => {
   return (
-    <div className="flex flex-1" id="start">
-      <div className="w-2/3">
-        {/* Content for the Start component */}
-        <div className='flex flex-col h-[600px] bg-blue-gray-600'>Start</div>
+    <>
+      {/* Black screen with text and GIF animation */}
+      <motion.div
+        className="fixed inset-0 flex flex-col items-center justify-start bg-black text-white z-10" // Changed justify-center to justify-start
+        initial={{ opacity: 1, x: 0 }} // Initial position (visible and centered)
+        animate={{ opacity: 1, x: '100%' }} // Animation to slide offscreen to the right
+        transition={{ duration: 0.8, delay: 1.5 }} // Transition duration with 3 seconds delay
+      >
+        <h1 className="text-5xl mb-20 mt-28 ">Welcome!</h1> {/* Added margin-top */}
+        <img src={cat} alt="GIF Animation" />
+      </motion.div>
+
+      {/* Content for the Start and Portfolio components */}
+      <div className="w-full h-full bg-white">
+        <div className="flex flex-1" id="start">
+          <div className="w-2/3">
+            {/* Content for the Start component */}
+            <div className="flex flex-col h-[600px] bg-blue-gray-600">Start</div>
+          </div>
+          <div className="w-1/3">
+            {/* Content for the Portfolio component */}
+            <div className="flex flex-col h-[600px] bg-blue-gray-100">Portfolio</div>
+          </div>
+        </div>
       </div>
-      <div className="w-1/3">
-        {/* Content for the Portfolio component */}
-        <div className='flex flex-col h-[600px] bg-blue-gray-100'>Portfolio</div>
-      </div>
-    </div>
+    </>
   );
-}
+};
 
 export default StartPortfolio;
